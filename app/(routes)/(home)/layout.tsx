a11770c5cb808e64bd271c0e84e26bda3c687351
Luis Vanegas <lvanegas1429@gmail.com>
2024-08-19 13:42:01 -0600

    SidebarMenuMobile done

--- app/(routes)/(home)/layout.tsx
+++ app/(routes)/(home)/layout.tsx
@@ -1,5 +1,6 @@
 import { Logo } from '@/components/shared/Logo'
 import Sidebar from '@/components/shared/Sidebar/Sidebar'
+import { SidebarMobile } from '@/components/shared/SidebarMobile'
 import React from 'react'
 
 const HomeLayout = ({ children }: Readonly<{ children: React.ReactNode }>) => {
@@ -9,7 +10,7 @@ const HomeLayout = ({ children }: Readonly<{ children: React.ReactNode }>) => {
         <div className="py-1 text-white">
           <Logo />
         </div>
-        <div>SidebarMobile</div>
+        <SidebarMobile/>
       </div>
       <div className="flex h-full">
         <div className='max-w-lg hidden lg:flex h-full w-72 flex-col fixed bg-blue-800 px-4 text-white'>
