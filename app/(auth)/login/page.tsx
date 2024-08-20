32e7bf8df6b04b12634779e7c56fa1d056ffbcbb
Luis Vanegas <lvanegas1429@gmail.com>
2024-08-20 13:40:58 -0600

    Maquetacion de login page lista

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import { ImageAuth } from './_components/ImageAuth'
 
 const LoginPage = () => {
   return (
-    <div>LoginPage</div>
+    <div className='grid md:grid-cols-2 h-full max-h-screen overflow-hidden'>
+      <div className="flex justify-center h-full">
+        <div className="text-white flex flex-col items-center justify-center p-6">
+          <h1 className='text-blue-500 text-2xl text-center mb-5'>Imagine Security</h1>
+          <h2 className='text-4xl font-medium text-black'>Welcome back</h2>
+          <p className='text-center mt-4 mb-6 text-slate-400 text-sm'>Welcome back, please enter your credentials.</p>
+          <div className="bg-blue-500 p-4">Tabs form...</div>
+        </div>
+      </div>
+      <ImageAuth/>
+    </div>
   )
 }
 
